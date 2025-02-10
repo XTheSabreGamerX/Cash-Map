673615a50990be28fe01a622e6de99a1adf9db79
Juan Carlos T. Matias <112912002+XTheSabreGamerX@users.noreply.github.com>
2025-02-10 23:44:45 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/cashmap/OnBoarding.kt
@@ -0,0 +1,14 @@
+package com.example.cashmap
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OnBoarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        }
+    }
