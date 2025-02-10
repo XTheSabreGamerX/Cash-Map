4eff4fd894ad7f2ee490c742a84d448c05a4f11c
XTheSabreGamerX <jhaycee081602@gmail.com>
2025-02-11 00:26:04 +0800

    Completed OnBoarding Activity

--- app/src/main/java/com/example/cashmap/OnBoarding.kt
+++ app/src/main/java/com/example/cashmap/OnBoarding.kt
@@ -10,5 +10,6 @@ class OnBoarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_on_boarding)
         }
     }
